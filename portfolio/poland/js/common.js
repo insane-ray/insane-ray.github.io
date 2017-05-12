ab1910891ce3388a296396ecdce11629c65b1bc1
Insane Ray <insane4ray@gmail.com>
2017-05-12 09:23:09 +0300

    portfolio poland v1.4.3

--- portfolio/poland/js/common.js
+++ portfolio/poland/js/common.js
@@ -68,5 +68,17 @@ $(document).ready(function(){
 	  });
 	});
 
+/* --- Animate jQuery ancor ---*/
+
+$(document).ready(function(){
+    $('.search-place-a').click( function(){ // ловим клик по ссылке с классом go_to
+	var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
+        if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
+	    $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 600); // анимируем скроолинг к элементу scroll_el
+        }
+	    return false; // выключаем стандартное действие
+    });
+});
+
 });
 
