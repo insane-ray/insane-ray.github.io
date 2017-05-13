5f3fc769edd2a47b298fb7f9918b2268f2186606
Insane Ray <insane4ray@gmail.com>
2017-05-13 17:53:03 +0300

    portfolio poland v1.4.5

--- portfolio/poland/js/common.js
+++ portfolio/poland/js/common.js
@@ -25,60 +25,56 @@ searchClose.onclick = function () {
 	} , 400);
 }
 
-$(document).ready(function(){
+/* --- Owl Carousel 2 --- */
 
-	/* --- Owl Carousel 2 --- */
+var owl = $('.owl-carousel');
 
-	var owl = $('.owl-carousel');
-
-	$('.owl-carousel').owlCarousel({
-	loop:true,
-	margin:30,
-	center: true,
-	autoWidth:true,
-	});
-
-	$('.next').click(function() {
-		owl.trigger('next.owl.carousel', [300]);
-	})
-	$('.prev').click(function() {
-		owl.trigger('prev.owl.carousel', [300]);
-	})
-
-	/* --- Animate jQuery ancor ---*/
-
-	$('.search-place-a').click( function(){ // ловим клик по ссылке с классом go_to
-	var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
-	    if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
-	    $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 600); // анимируем скроолинг к элементу scroll_el
-	    }
-	    return false; // выключаем стандартное действие
-	});
-
-	/* --- Isotope --- */
-
-	setTimeout(function() {
-		var $grid = $('.place-to-go__grid').isotope({
-		filter: '.journey',
-		itemSelector: '.place-to-go__item',
-		layoutMode: 'fitRows',
-		});
-
-		// bind filter button click
-		$('#filters').on( 'click', 'button', function() {
-		  var filterValue = $( this ).attr('data-filter');
-		  $grid.isotope({ filter: filterValue });
-		});
-
-		// change is-checked class on buttons
-		$('.place-to-go__buttons').each( function( i, buttonGroup ) {
-		  var $buttonGroup = $( buttonGroup );
-		  $buttonGroup.on( 'click', 'button', function() {
-		    $buttonGroup.find('.is-checked').removeClass('is-checked');
-		    $( this ).addClass('is-checked');
-		  });
-		});
-	}, 700);
-
+$('.owl-carousel').owlCarousel({
+loop:true,
+margin:30,
+center: true,
+autoWidth:true,
 });
+
+$('.next').click(function() {
+	owl.trigger('next.owl.carousel', [300]);
+})
+$('.prev').click(function() {
+	owl.trigger('prev.owl.carousel', [300]);
+})
+
+/* --- Animate jQuery ancor ---*/
+
+$('.search-place-a').click( function(){ // ловим клик по ссылке с классом go_to
+var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
+    if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
+    $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 600); // анимируем скроолинг к элементу scroll_el
+    }
+    return false; // выключаем стандартное действие
+});
+
+/* --- Isotope --- */
+
+setTimeout(function() {
+	var $grid = $('.place-to-go__grid').isotope({
+	filter: '.journey',
+	itemSelector: '.place-to-go__item',
+	layoutMode: 'fitRows',
+	});
+
+	// bind filter button click
+	$('#filters').on( 'click', 'button', function() {
+	  var filterValue = $( this ).attr('data-filter');
+	  $grid.isotope({ filter: filterValue });
+	});
+
+	// change is-checked class on buttons
+	$('.place-to-go__buttons').each( function( i, buttonGroup ) {
+	  var $buttonGroup = $( buttonGroup );
+	  $buttonGroup.on( 'click', 'button', function() {
+	    $buttonGroup.find('.is-checked').removeClass('is-checked');
+	    $( this ).addClass('is-checked');
+	  });
+	});
+}, 700);
 
